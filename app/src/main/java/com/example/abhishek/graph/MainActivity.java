ad16089801554bb49ee54cc7dcb6bae9efddb7fa
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2017-08-12 22:50:15 +0530

    runtime graph

--- app/src/main/java/com/example/abhishek/graph/MainActivity.java
+++ app/src/main/java/com/example/abhishek/graph/MainActivity.java
@@ -1,37 +1,74 @@
 package com.example.abhishek.graph;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.util.Timer;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Handler mHandler = new Handler();
     LineGraphSeries<DataPoint> series;
     GraphView graphView;
-    double x, y;
 
+    private Runnable mTimer2;
+    private double graph2LastXValue = 5d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        x = 0;
+        series = new LineGraphSeries<>();
 
         graphView = (GraphView) findViewById(R.id.graph);
-        series = new LineGraphSeries<DataPoint>();
-
-        for (int i = 0; i< 500; i++) {
-            x = x + 0.1;
-            y = Math.sin(x);
-            series.appendData(new DataPoint(x, y), true, 1000);
-        }
         graphView.addSeries(series);
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(20);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTimer2 = new Runnable() {
+            @Override
+            public void run() {
+                graph2LastXValue += 1d;
+                series.appendData(new DataPoint(graph2LastXValue, getRandom()), true, 40);
+                mHandler.postDelayed(this, 200);
+            }
+        };
+        mHandler.postDelayed(mTimer2, 1000);
+    }
+
+    @Override
+    public void onPause() {
+        mHandler.removeCallbacks(mTimer2);
+        super.onPause();
+    }
+
+    private DataPoint[] generateData() {
+        int count = 30;
+        DataPoint[] values = new DataPoint[count];
+        for (int i=0; i<count; i++) {
+            double x = i;
+            double f = mRand.nextDouble()*0.15+0.3;
+            double y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
+            DataPoint v = new DataPoint(x, y);
+            values[i] = v;
+        }
+        return values;
+    }
+
+    double mLastRandom = 2;
+    Random mRand = new Random();
+    private double getRandom() {
+        return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
     }
 }
